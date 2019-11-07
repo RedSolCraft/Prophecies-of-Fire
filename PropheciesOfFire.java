824d3867a6e307005f886b99c497f89aa65fc6c5
RedSolCraft <57502226+RedSolCraft@users.noreply.github.com>
2019-11-07 12:14:41 -0700

    Update PropheciesOfFire.java

--- PropheciesOfFire.java
+++ PropheciesOfFire.java
@@ -1,9 +1,11 @@
 package propheciesoffire;
 
-public class PropheciesOfFire {
-
-    public static void main(String[] args) {
-
+public class PropheciesOfFire
+{
+    
+    public static void main(String[] args)
+    {
+        
     }
     System.out.println("Hello!");
 }
