bb757c67f6e3358e3b7c834f3761915f394c54e1
RedSolCraft <57502226+RedSolCraft@users.noreply.github.com>
2019-11-07 12:10:49 -0700

    Add files via upload
    SuperClass for Game

---
+++ PropheciesOfFire.java
@@ -0,0 +1,16 @@
+package propheciesoffire;
+
+/**
+ *
+ * @author Jared Clark
+ */
+public class PropheciesOfFire {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
