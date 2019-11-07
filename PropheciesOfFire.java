1d819617425e03d15db6f06ca29c7b96c9785d74
Sergi0m <57502974+Sergi0m@users.noreply.github.com>
2019-11-07 12:13:59 -0700

    Update PropheciesOfFire.java

--- PropheciesOfFire.java
+++ PropheciesOfFire.java
@@ -1,16 +1,9 @@
 package propheciesoffire;
 
-/**
- *
- * @author Jared Clark
- */
 public class PropheciesOfFire {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
     System.out.println("Hello!");
 }
